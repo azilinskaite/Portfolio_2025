23edbc8f1738eba3c6d63481230558d7ca057680
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2025-03-16 21:17:22 +0100

    Styling pdates

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovingHeader from "./Components/MovingHeaderComponent/MovingHeader";
 import Project from "./Components/ProjectComponent/Project";
 import Footer from "./Components/FooterComponent/Footer";
 import cocktailPhoto from "../src/Assets/cocktail-kungen-mockup.png";
-// import chaosPhoto from "../src/Assets/chaos-magazine.png";
+import chaosPhoto from "../src/Assets/chaos-magazine-mockup.png"
 
 function App() {
   return (
@@ -15,9 +15,9 @@ function App() {
       <MovingHeader />
       <section className="projects-container" id="work">
         <Project photo={cocktailPhoto} title="Cocktail Kungen" info="Cocktail recipes app. Using external API and React.js" />
-        <Project photo={cocktailPhoto} title="Chaos magazine" info="Mock up of a magazine website with functioning music quiz game. Built using external API, JavaScript, CSS & HMTL" />
+        <Project photo={chaosPhoto} title="Chaos magazine" info="Mock up of a magazine website with functioning music quiz game. Built using external API, JavaScript, CSS & HMTL" />
         <Project photo={cocktailPhoto} title="Bazaar Bab" info="Landing page for a restaurant built using JavaSript, CSS, HTML." />
-        <Project photo={cocktailPhoto} title="Library App" info="JavaSript project showing CRUD functionality, adding, updating and deleting books in a personal library." />
+        <Project photo={chaosPhoto} title="Library App" info="JavaSript project showing CRUD functionality, adding, updating and deleting books in a personal library." />
       </section>
       <Footer/>
     </div>
--- src/Components/MovingHeaderComponent/MovingHeader.jsx
+++ src/Components/MovingHeaderComponent/MovingHeader.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import "./MovingHeaderStyles.css";
 
 const MovingHeader = () => {
-    return (
-        <h3 className="moving-header">Selected work</h3>
-    );
-}
+  return (
+    <div className="marquee-wrapper">
+      <div className="marquee-track">
+        <h3 className="marquee-text">Selected work</h3>
+        <h3 aria-hidden="true" className="marquee-text">Selected work</h3>
+        <h3 aria-hidden="true" className="marquee-text">Selected work</h3>
+        <h3 aria-hidden="true" className="marquee-text">Selected work</h3>
+        <h3 aria-hidden="true" className="marquee-text">Selected work</h3>
+        <h3 aria-hidden="true" className="marquee-text">Selected work</h3>
+      </div>
+    </div>
+  );
+};
 
 export default MovingHeader;
